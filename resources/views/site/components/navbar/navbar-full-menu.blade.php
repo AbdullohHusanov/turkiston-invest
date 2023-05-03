60a9f91e9fa1a035536299e4f7242fec8ad4e7d5
Abdulloh Husanov <husanova@aslim.uz>
2023-05-03 15:26:41 +0500

    menus have been made dynamic

--- resources/views/site/components/navbar/navbar-full-menu.blade.php
+++ resources/views/site/components/navbar/navbar-full-menu.blade.php
@@ -2,43 +2,52 @@
     <div class="full-menu-bg"></div>
     <div class="container">
         <div class="full-menu-row">
-            <div class="full-menu-col">
-                <ul class="full-menu-ul">
-                    <li  class="full-menu-li">
-                        <div class="flex items-center justify-between">
-                            <a id="menuItem" class="full-menu-link"  href="#">test</a>
-                            <span class="chevron-down" id="arrowMenu"></span>
-                        </div>
-                        <ul id="mobileSubmenu" class="mobile-submenu-ul hidden md:block">
-                            <li class="mobile-submenu-li full-menu-li"><a class="mobile-submenu-li full-menu-link" href="#">sm submenu</a> </li>
-                            <li class="mobile-submenu-li full-menu-li"><a class="mobile-submenu-li full-menu-link" href="#">sm submenu</a> </li>
-                            <li class="mobile-submenu-li full-menu-li"><a class="mobile-submenu-li full-menu-link" href="#">sm submenu</a> </li>
-                            <li class="mobile-submenu-li full-menu-li"><a class="mobile-submenu-li full-menu-link" href="#">sm submenu</a> </li>
+            @foreach($full_menu_elements as $full_menu_element)
+                @if(!count($full_menu_element['children']))
+                    <div class="full-menu-col">
+                        <ul class="full-menu-ul">
+                            <li class="full-menu-li">
+                                <a class="full-menu-link"
+                                   href="{{ $full_menu_element['value'] }}">{{ $full_menu_element['name'] }}</a>
+                            </li>
                         </ul>
-                    </li>
-                    <li class="full-menu-li">
-                        <a class="full-menu-link" href="#">test</a>
-                    </li>
-                    <li class="full-menu-li">
-                        <a class="full-menu-link" href="#">test</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="full-menu-col">
-                <ul class="full-menu-ul">
-                    <li class="full-menu-li">
-                        <a class="full-menu-link" href="#">test</a>
-                    </li>
-                    <li class="full-menu-li">
-                        <a class="full-menu-link" href="#">test</a>
-                    </li>
-                    <li class="full-menu-li">
-                        <a class="full-menu-link" href="#">test</a>
-                    </li>
-                </ul>
-            </div>
-
+                    </div>
+                @else
+                    <div class="full-menu-col">
+                        <ul class="full-menu-ul">
+                            <li class="full-menu-li">
+                                <a class="full-menu-link"
+                                   href="{{ $full_menu_element['value'] }}">{{ $full_menu_element['name'] }}</a>
+                                @foreach($full_menu_element['children'] as $full_menu_el)
+                                    @if(!count($full_menu_el['children']))
+                                        <ul id="mobileSubmenu" class="mobile-submenu-ul hidden md:block">
+                                            <li class="mobile-submenu-li full-menu-li"><a
+                                                    class="mobile-submenu-li full-menu-link"
+                                                    href="{{ $full_menu_el['value'] }}">{{ $full_menu_el['name'] }}</a>
+                                            </li>
+                                        </ul>
+                                    @else
+                                        <ul id="mobileSubmenu" class="mobile-submenu-ul hidden md:block">
+                                            <li class="mobile-submenu-li full-menu-li ml-3">
+                                                <a class="mobile-submenu-li full-menu-link"
+                                                   href="{{ $full_menu_el['value'] }}">{{ $full_menu_el['name'] }}</a>
+                                                @foreach($full_menu_el['children'] as $full_menu_i)
+                                                    <ul id="mobileSubmenu" class="mobile-submenu-ul hidden md:block">
+                                                        <li class="mobile-submenu-li full-menu-li"><a
+                                                                class="mobile-submenu-li full-menu-link"
+                                                                href="{{ $full_menu_i['value'] }}">{{ $full_menu_i['name'] }}</a>
+                                                        </li>
+                                                    </ul>
+                                                @endforeach
+                                            </li>
+                                        </ul>
+                                    @endif
+                                @endforeach
+                            </li>
+                        </ul>
+                    </div>
+                @endif
+            @endforeach
         </div>
     </div>
 </div>
